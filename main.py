d30c06469c4d74d164d8c96d85d8f16d3b7f1c7a
max <79382254+maxcah@users.noreply.github.com>
2021-08-05 22:10:02 -0400

    Rewrote using Genius API

--- main.py
+++ main.py
@@ -1,43 +1,58 @@
 from bs4 import BeautifulSoup
 import requests
-import time
 import matplotlib.pyplot as plt
+import re
 
-all_lyrics = ''
+def get_info(artist_name, page):
+    base_url = 'https://api.genius.com'
+    access_token = 'u9Y8tPQWDHXFRKSSYArtObxN1gbc_HTdyfx4y3rMyVb6jJOCPA3qkHVI4SNC3Pcx'
+    headers = {'Authorization': 'Bearer ' + access_token}
+    search_url = base_url + '/search?per_page=10&page=' + str(page)
+    data = {'q': artist_name}
+    response = requests.get(search_url, data=data, headers=headers)
+    return response
 
-song_urls = []	
-html_azlyrics = requests.get("https://www.azlyrics.com/c/clairo.html").text
-soup = BeautifulSoup(html_azlyrics, 'html.parser')
-songs = soup.find_all('div', class_='listalbum-item')
-for div in songs:
-    for a in div.find_all('a'):
-        song_urls.append((a['href'][3:]))
 
-for url in song_urls:
-	if url[0] == 'l':
-	    html_song = requests.get(f"https://www.azlyrics.com/{url}").text
-	    soup_2 = BeautifulSoup(html_song, 'html.parser')
-	    lyrics = soup_2.find_all('div')[11]
-	    print(url)
-	    for lyric in (lyrics.find_all('div')[8].get_text().encode('ascii', 'ignore').decode()):
-	    	all_lyrics += lyric
-	    time.sleep(3)
-	else:
-		pass
+def get_urls(artist_name):
+    page = 1
+    num_of_songs = [0]
+    song_urls = []
 
-word_frequency = {}
+    while True:
+        response = get_info(artist_name, page)
+        json = response.json()
 
-for word in all_lyrics.replace("'", '').replace("?", '').replace('!', '').replace('!', '').replace('"', '').split():
-    if word in word_frequency:
-        word_frequency[word] += 1
-    else:
-        word_frequency[word] = 1
+        song_info = []
+        for hit in json['response']['hits']:
+            if artist_name.lower() in hit['result']['primary_artist']['name'].lower():
+                    song_info.append(hit)
+        
 
-sort_word_frequency = sorted(word_frequency.items(), key=lambda x: x[-1], reverse=True)
-print(sort_word_frequency)
 
-lyrics_plot = plt.bar([value[0] for value in sort_word_frequency[:9]], [value[1] for value in sort_word_frequency[:9]])
-plt.xlabel('Lyrics')
-plt.ylabel('# of Occurrences')
-plt.show()
+        for song in song_info:
+                song_urls.append(song['result']['url'])
+        print(len(song_urls))
 
+        num_of_songs.append(len(song_urls))
+        if num_of_songs[-1] == num_of_songs[-2]:
+            break
+        else:
+            page += 1
+
+    return song_urls
+
+
+def scrape_lyrics(url):
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text, 'html.parser')
+    lyrics = soup.find_all('div', class_='Lyrics__Container-sc-1ynbvzw-8 eOLwDW')
+    lyrics = ''.join(re.sub('\[(.*?)\]', '', section.get_text(" ")) for section in lyrics)
+    return lyrics
+
+def write_lyrics_to_file(artist_name):
+    f = open(f'{artist_name}.txt', 'w+', encoding='utf-8')
+    urls = get_urls(artist_name)
+    for url in urls:
+        lyrics = scrape_lyrics(url)
+        f.write(lyrics)
+    f.close()
